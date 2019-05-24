f010da204208293f0a9bb2b0c88583129a34d98b
JakinHuangWang <31380172+JakinHuangWang@users.noreply.github.com>
2019-05-24 12:31:30 -0700

    05/24/2019 12:31 Update

--- Lab4thread.py
+++ Lab4thread.py
@@ -7,6 +7,7 @@ import collections
 import json
 import threading
 import tkinter as tk
+import tkinter.messagebox as tkmb
 
 with open('states_hash.json', 'r') as infile:
     State_dict = json.load(infile)
@@ -22,8 +23,11 @@ class mainWin(tk.Tk):
         self.LB.grid(row = 1, column = 0)
         for v in State_dict.values():
             self.LB.insert(tk.END, v)
-        tk.Button(self, text='OK', command=lambda:disWin(self).display(self, [codeLst[i] for i in self.LB.curselection()], 
-                                                                       [nameLst[i] for i in self.LB.curselection()])).grid(row=2, column=0)
+        tk.Button(self, text='OK', command=lambda:tkmb.showerror("Input Error", "Has to be between 1 to 3 options selected", parent=self)
+                  if len(self.LB.curselection()) > 3 or len(self.LB.curselection()) < 1 
+                  else disWin(self).display(self, [codeLst[i] for i in self.LB.curselection()],
+                                      [nameLst[i] for i in self.LB.curselection()])).grid(row=2, column=0)
+            
         
 class disWin(tk.Toplevel):
     def __init__(self, master):
@@ -38,12 +42,11 @@ class disWin(tk.Toplevel):
             print(codeLst[i], nameLst[i])
             t = threading.Thread(target=self.loadFromWeb, args=(codeLst[i],), name=nameLst[i])
             threadLst.append(t)
-        for t in threadLst:
-            t.start()
-        outStr = ''
         i = 3
-        for t in threadLst:     
+        for t in threadLst: 
+            t.start()
             t.join()
+            tk.Label(master, text='').grid(row=i, column=0)                        
             tk.Label(master, text=t.getName() + ": " + self.data['total']).grid(row=i, column=0)            
             for d in self.data['data']:
                 self.LB.insert(tk.END, d['name'] + ' ' + d['designation'])            
