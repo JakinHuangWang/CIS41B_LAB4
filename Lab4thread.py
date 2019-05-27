83b150879d9b6e0001f36cc155b772c76099a9a2
JakinHuangWang <31380172+JakinHuangWang@users.noreply.github.com>
2019-05-27 13:04:23 -0700

    05/27/2019 13:04 Update

--- Lab4thread.py
+++ Lab4thread.py
@@ -29,22 +29,21 @@ class mainWin(tk.Tk):
                   if len(self.LB.curselection()) > 3 or len(self.LB.curselection()) < 1 
                   else disWin(self).display(self, [codeLst[i] for i in self.LB.curselection()],
                                       [nameLst[i] for i in self.LB.curselection()])).grid(row=2, column=0)
-            
         
 class disWin(tk.Toplevel):
     def __init__(self, master):
         super().__init__(master)
-        self.data = {}
-        self.label = tk.Label(master, text='')
-        self.label.grid(row = 3, column = 0)        
+        self.data = []       
         self.LB = tk.Listbox(self, width=50, height=10, selectmode = 'multiple', yscrollcommand = tk.Scrollbar(self).set)
         self.LB.grid(row = 0, column = 0)
         tk.Button(self, text='OK', command=self.browseFile).grid(row=1, column=0)
-        
+        self.label = tk.Label(master, text='')
+        self.label.grid(row = 3, column = 0)         
+        self.protocol("WM_DELETE_WINDOW", self.eraseLabel)
+    def eraseLabel(self):
+        self.label['text'] = ''
+        self.destroy()
     def display(self, master, codeLst, nameLst):
-        self.label['text'] = str()
-        self.label.update()
-        time.sleep(5)
         threadLst = []
         for i in range(len(codeLst)):
             print(codeLst[i], nameLst[i])
@@ -55,10 +54,10 @@ class disWin(tk.Toplevel):
             t = threadLst[i]
             t.start()
             t.join()
-            output += t.getName() + ": " + self.data['total'] + '\t'
+            output += t.getName() + ": " + self.data[i]['total'] + '\t'
             self.label['text'] = output
             self.label.update() 
-            for d in self.data['data']:
+            for d in self.data[i]['data']:
                 self.LB.insert(tk.END, nameLst[i] + ': ' + d['name'] + ' ' + d['designation'])            
                 
     def loadFromWeb(self, StateCode):
@@ -66,19 +65,27 @@ class disWin(tk.Toplevel):
         with lock:
             page = requests.get("https://developer.nps.gov/api/v1/parks?stateCode=" + 
                                 StateCode + "&api_key=" + API_key)
-            self.data = page.json()
+            self.data.append(page.json())
             
     def browseFile(self):
-        default_path = os.getcwd()
         filename = filedialog.askdirectory()
+        if filename == '':
+            return
+        if os.path.isfile(filename + '/parks.txt'):
+            tkmb.showinfo("Browse File Notification", "parks.txt already existed and will be overwritten", parent = self)
+            filename += '/parks.txt'
+        if  filename == os.getcwd():
+            filename = 'parks.txt'
         try:
-            with open(filename + '/parks.txt', 'w') as outfile:
-                for k, v in self.data.items():
-                    outfile.write(str(v))
+            with open(filename, 'w', encoding="utf-8") as outfile:
+                for dic in self.data:
+                    for d in dic['data']:
+                        outfile.write(d['fullName'] + '; ' + 
+                                      ', '.join([(State_dict[state] if state in State_dict else state) for state in d['states'].split(',')]) 
+                                      + '\n' + d['description'] + '\n\n')
         except FileNotFoundError:
-            with open(default_path + '/parks.txt', 'w') as outfile:
-                for k, v in self.data.items():
-                    outfile.write(str(v))                
+            raise SystemExit
+        self.destroy()
             
 mWin = mainWin()
 mWin.mainloop()
