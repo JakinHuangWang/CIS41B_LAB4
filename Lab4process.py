9b817c80fb7863816c74cc91dfb440beb9d81e13
JakinHuangWang <31380172+JakinHuangWang@users.noreply.github.com>
2019-05-28 18:07:17 -0700

    Nearly Final Update(Only Needs to add comments)

--- Lab4process.py
+++ Lab4process.py
@@ -5,8 +5,6 @@ Description: Lab4 uses the NPS to create a GUI and utilizing processes
 that allows the users to view all the parks in a certain state
 '''
 import requests
-from bs4 import BeautifulSoup 
-import sys
 import os
 import json
 import tkinter as tk, multiprocessing as mp
@@ -16,9 +14,12 @@ from tkinter import filedialog
 API_key = "11hZnAoJ398zWsTjp6HhqCIhlblofgLwSUsB1EJg"
 
 def loadFromWeb(StateCode):
-    page = requests.get('https://developer.nps.gov/api/v1/parks?stateCode='+StateCode+'&api_key='+API_key)
-    return page.json()['data']
+    lock = mp.Lock()
+    with lock:
+        page = requests.get('https://developer.nps.gov/api/v1/parks?stateCode='+StateCode+'&api_key='+API_key)
+        return page.json()
     
+#The Main Window creates a listbox which allows the users to choose 1 - 3 states to view the parks.
 class mainWin(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -26,63 +27,72 @@ class mainWin(tk.Tk):
             State_dict = json.load(infile)
             self.codeLst = list(State_dict.keys())
             self.nameLst = list(State_dict.values())
+        self.data = []
+        self.title("US National Parks")
         tk.Label(self, text="Select up to 3 States").grid(row = 0, column = 0)
-        self.title('Main Window')
         self.LB = tk.Listbox(self, height = 10, selectmode = 'multiple', yscrollcommand = tk.Scrollbar(self).set)
         self.LB.grid(row = 1, column = 0)
         self.LB.insert(tk.END, *self.nameLst)
-        tk.Button(self, text='OK', command=lambda:disWin(self).display(self, [self.codeLst[i] for i in self.LB.curselection()],
-                [self.nameLst[i] for i in self.LB.curselection()]) if  1 <= len(self.LB.curselection()) <= 3
-                else  tkmb.showerror("Input Error", "Has to be between 1 to 3 options selected", parent=self)).grid(row=2, column=0)
+        self.label = tk.Label(self, text='')
+        self.label.grid(row = 3, column = 0) 
+        tk.Button(self, text='OK', command=lambda:self.fetch([self.codeLst[i] for i in self.LB.curselection()],
+                    [self.nameLst[i] for i in self.LB.curselection()]) if 1 <= len(self.LB.curselection()) <= 3
+                  else tkmb.showerror("Input Error", "Has to be between 1 to 3 options selected", parent=self)).grid(row=2, column=0)
         
+    def fetch(self, codeSelect, nameSelect):
+        self.label['text'] = 'Fetching data for ' + str(len(codeSelect)) + ' States(s)'
+        self.label.update()        
+        pool = mp.Pool(len(codeSelect))
+        self.data = pool.map(loadFromWeb, codeSelect)
+        self.label['text'] = ''
+        self.label.update()      
+        disWin(self).display([(nameSelect[i], dic) for i in range(len(self.data)) for dic in self.data[i]['data']])
+        
+#The Display Window displays all the state parks chosen by the user     
 class disWin(tk.Toplevel):
     def __init__(self, master):
         super().__init__(master)
+        self.data = []
+        self.title('Display Window')
         self.LB = tk.Listbox(self, width=50, height=10, selectmode = 'multiple', yscrollcommand = tk.Scrollbar(self).set)
         self.LB.grid(row = 0, column = 0)
-        tk.Button(self, text='OK', command=self.browseFile).grid(row=1, column=0)
-        self.title('Display Window')
-        self.label = tk.Label(master, text='')
-        self.label.grid(row = 3, column = 0)         
-        self.protocol("WM_DELETE_WINDOW", self.eraseLabel)
-        self.focus_set()
+        tk.Button(self, text='OK', command=lambda: self.browseAndWriteFile() if len(self.LB.curselection()) >= 1
+                    else tkmb.showerror("Error Message", "Please select at least 1 choice")).grid(row=1, column=0)     
+        self.protocol("WM_DELETE_WINDOW", lambda:self.eraseLabel(master))
         self.grab_set()
-        
-    def eraseLabel(self):
-        self.label['text'] = ''
+        self.focus_set()
+    
+    #clear the label and destroy the display window    
+    def eraseLabel(self, master):
+        master.label['text'] = ''
+        master.label.update()
         self.destroy()
         
-    def display(self, master, codeSelect, nameSelect):
-        self.codeSelect = codeSelect
-        self.nameSelect = nameSelect
-        pool = mp.Pool(len(codeSelect))
-        self.data = pool.map(loadFromWeb, codeSelect)
-        self.label['text'] = 'Fetching data for ' + str(len(codeSelect)) + ' States(s)'
-        self.label.update()
-        print(self.data)
-        for i in range(len(codeSelect)):
-            self.LB.insert(tk.END, *[nameSelect[i] + ': ' + d['fullName'] for d  in self.data[i]])
-            
-    def browseFile(self):
+    #Display all the state parks by using threads to scrap the web    
+    def display(self, data):
+        self.data = data
+        self.LB.insert(tk.END, *[tup[0] + ': ' + tup[1]['fullName'] for tup in self.data])            
+    
+    #Allows the users to choose a directory to write the parks.txt        
+    def browseAndWriteFile(self):
+        choices = [(tup[0], tup[1]['fullName']) for tup in self.data]
         filename = filedialog.askdirectory()
         if filename == '':
             return
-        if os.path.isfile(filename + 'parks.txt'):
+        if os.path.isfile(filename + '/parks.txt'):
             tkmb.showinfo("Browse File Notification", "parks.txt already existed and will be overwritten", parent = self)
+        filename += '/parks.txt'
         if  filename == os.getcwd():
-            filename = 'parks.txt'
-        else:
-            filename += 'parks.txt'
+            filename = '/parks.txt'
         try:
             with open(filename, 'w', encoding="utf-8") as outfile:
-                for i in range(len(self.data)):
-                    outfile.writelines([dic['fullName'] + '; ' + self.nameSelect[i] 
-                                      + '\n' + dic['description'] + '\n\n' for dic in self.data[i]] )
+                outfile.writelines([self.data[num][1]['fullName'] + '; ' + self.data[num][0]
+                                      + '\n' + self.data[num][1]['description'] + '\n\n' 
+                                      for num in self.LB.curselection()])
         except FileNotFoundError:
             raise SystemExit
         self.destroy()
 
-    
-if __name__ == '__main__':
+if __name__ == '__main__':            
     mWin = mainWin()
     mWin.mainloop()
--- Lab4thread.py
+++ Lab4thread.py
@@ -5,17 +5,14 @@ Description: Lab4 uses the NPS to create a GUI and utilizing threading
 that allows the users to view all the parks in a certain state
 '''
 import requests
-from bs4 import BeautifulSoup 
 import json
+import os
 import threading
 from tkinter import filedialog
 import tkinter as tk
 import tkinter.messagebox as tkmb
-import os
 
-#Be reading from states_hash.json we can create two lists of State Names and State Codes
-
-
+#This is my Key for the NPS API
 API_key = "11hZnAoJ398zWsTjp6HhqCIhlblofgLwSUsB1EJg"
 
 #The Main Window creates a listbox which allows the users to choose 1 - 3 states to view the parks.
@@ -26,16 +23,44 @@ class mainWin(tk.Tk):
             State_dict = json.load(infile)
             self.codeLst = list(State_dict.keys())
             self.nameLst = list(State_dict.values())
-        self.title("Main Window")
+        self.data = []
+        self.title("US National Parks")
         tk.Label(self, text="Select up to 3 States").grid(row = 0, column = 0)
         self.LB = tk.Listbox(self, height = 10, selectmode = 'multiple', yscrollcommand = tk.Scrollbar(self).set)
         self.LB.grid(row = 1, column = 0)
         self.LB.insert(tk.END, *self.nameLst)
-        tk.Button(self, text='OK', command=lambda:tkmb.showerror("Input Error", "Has to be between 1 to 3 options selected", parent=self)
-                  if len(self.LB.curselection()) > 3 or len(self.LB.curselection()) < 1 
-                  else disWin(self).display(self, [self.codeLst[i] for i in self.LB.curselection()],
-                                      [self.nameLst[i] for i in self.LB.curselection()])).grid(row=2, column=0)
+        self.label = tk.Label(self, text='')
+        self.label.grid(row = 3, column = 0) 
+        tk.Button(self, text='OK', command=lambda:self.fetch([self.codeLst[i] for i in self.LB.curselection()],
+                                      [self.nameLst[i] for i in self.LB.curselection()]) if 1 <= len(self.LB.curselection()) <= 3
+                                    else tkmb.showerror("Input Error", "Has to be between 1 to 3 options selected", parent=self)).grid(row=2, column=0)
 
+    def fetch(self, codeSelect, nameSelect):
+        self.data.clear()
+        threadLst = []
+        self.label['text'] = ''
+        self.label.update()        
+        for i in range(len(codeSelect)):
+            t = threading.Thread(target=self.loadFromWeb, args=(codeSelect[i], nameSelect[i]), name=nameSelect[i])
+            threadLst.append(t)
+        output = 'Result: '
+        for i in range(len(threadLst)): 
+            t = threadLst[i]
+            t.start()
+            t.join()
+            output += t.getName() + ": " + self.data[i][1]['total'] + '\t'
+            self.label['text'] = output
+            self.label.update()
+        disWin(self).display(self.data)
+        
+    #Loab data from the web using NPS API           
+    def loadFromWeb(self, StateCode, StateName):
+        lock = threading.Lock()
+        with lock:
+            page = requests.get("https://developer.nps.gov/api/v1/parks?stateCode=" + 
+                                StateCode + "&api_key=" + API_key)
+            self.data.append((StateName, page.json()))
+            
 #The Display Window displays all the state parks chosen by the user     
 class disWin(tk.Toplevel):
     def __init__(self, master):
@@ -45,40 +70,22 @@ class disWin(tk.Toplevel):
         self.LB = tk.Listbox(self, width=50, height=10, selectmode = 'multiple', yscrollcommand = tk.Scrollbar(self).set)
         self.LB.grid(row = 0, column = 0)
         tk.Button(self, text='OK', command=lambda: self.browseAndWriteFile() if len(self.LB.curselection()) >= 1
-                    else tkmb.showerror("Error Message", "Please select at least 1 choice")).grid(row=1, column=0)
-        self.label = tk.Label(master, text='')
-        self.label.grid(row = 3, column = 0)         
-        self.protocol("WM_DELETE_WINDOW", self.eraseLabel)
+                    else tkmb.showerror("Error Message", "Please select at least 1 choice")).grid(row=1, column=0)     
+        self.protocol("WM_DELETE_WINDOW", lambda:self.eraseLabel(master))
         self.grab_set()
         self.focus_set()
     
     #clear the label and destroy the display window    
-    def eraseLabel(self):
-        self.label['text'] = ''
-        self.label.update()
+    def eraseLabel(self, master):
+        master.label['text'] = ''
+        master.label.update()
         self.destroy()
         
     #Display all the state parks by using threads to scrap the web    
-    def display(self, master, codeSelect, nameSelect):
-        threadLst = []
-        self.label['text'] = ''
-        self.label.update()        
-        self.dataSelect = []
-        self.codeSelect = codeSelect
-        self.nameSelect = nameSelect
-        for i in range(len(self.codeSelect)):
-            t = threading.Thread(target=self.loadFromWeb, args=(self.codeSelect[i], ), name=self.nameSelect[i])
-            threadLst.append(t)
-        output = 'Result: '
-        for i in range(len(threadLst)): 
-            t = threadLst[i]
-            t.start()
-            t.join()
-            print(self.data[i])
-            output += t.getName() + ": " + self.data[i]['total'] + '\t'
-            self.label['text'] = output
-            self.label.update()
-            self.LB.insert(tk.END, *[t.getName() + ': ' + d['fullName'] for d in self.data[i]['data']])            
+    def display(self, data):
+        self.data = data
+        for tup in self.data:
+            self.LB.insert(tk.END, *[tup[0] + ': ' + d['fullName'] for d in tup[1]['data']])            
     
     #Loab data from the web using NPS API           
     def loadFromWeb(self, StateCode):
@@ -88,11 +95,9 @@ class disWin(tk.Toplevel):
                                 StateCode + "&api_key=" + API_key)
             self.data.append(page.json())
             
-    
     #Allows the users to choose a directory to write the parks.txt        
     def browseAndWriteFile(self):
-        choices = [(self.nameSelect[i], d) for i in range(len(self.data)) for d in self.data[i]['data'] ]
-        print(choices)
+        choices = [(self.data[i][0], d) for i in range(len(self.data)) for d in self.data[i][1]['data'] ]
         filename = filedialog.askdirectory()
         if filename == '':
             return
